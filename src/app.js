5ce666cc48e9039dbde9d9e0f54b313ad1650ea7
Swathi <swathiganiga98@gmail.com>
2026-02-02 15:53:41 +0530

    exploring crud operation

--- src/app.js
+++ src/app.js
@@ -6,7 +6,18 @@ app.listen(3000,(()=>{
     console.log('Server is running on port 3000');
 }))
 
-app.use("/",(req,res)=>{
-    res.send("Hello World from Express.js dashboard!");
-})
 
+
+app.use("/hello",(req,res)=>{
+    res.send("hello");
+})
+// app.use("/",(req,res)=>{
+//     res.send("Hello World from Express.js dashboard!");
+// })
+
+app.get("/user",(req,res)=>{
+    res.json({name:"John", age:30});
+})
+app.post("/user",(req,res)=>{
+    res.json({status:"Data received"});
+})
