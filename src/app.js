e3082c40e1e9297113daf376d9695331270386a0
Swathi <swathiganiga98@gmail.com>
2026-02-04 10:52:11 +0530

    database initialzation

--- src/app.js
+++ src/app.js
@@ -1,23 +1,54 @@
-const express =require('express');
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/database');
+const User = require('./modals/User');
+const app= express();
+app.post('/signup', async(req,res)=>{
 
-const app=express();
+    const user = new User({
+        firstName: "sridhara",
+        lastName: "ganapathi",
+        email: "sri@gmail.com",
+        password: "sri123",
+       
+    })
+    try{
+        console.log('Saving user:', user);
+await user.save();
+res.send( 'User created successfully');
+    }catch(err){
+        console.error('Error saving user:', err);
+        res.status(500).send(err);
+        return;
+    }
+   
+})
 
-app.listen(3000,(()=>{
-    console.log('Server is running on port 3000');
-}))
+connectDB().then(() => {
+    console.log('Connected to the database successfully');
+    app.listen(3000, (() => {
+        console.log('Server is running on port 3000');
+    }))
+}).catch((err) => {
+    console.error('Failed to connect to the database:', err);
+    // process.exit(1); // Exit the application if the database connection fails
+});
 
 
 
-app.use("/hello",(req,res)=>{
+
+
+
+app.use("/hello", (req, res) => {
     res.send("hello");
 })
 // app.use("/",(req,res)=>{
 //     res.send("Hello World from Express.js dashboard!");
 // })
 
-app.get("/user",(req,res)=>{
-    res.json({name:"John", age:30});
+app.get("/user", (req, res) => {
+    res.json({ name: "John", age: 30 });
 })
-app.post("/user",(req,res)=>{
-    res.json({status:"Data received"});
+app.post("/user", (req, res) => {
+    res.json({ status: "Data received" });
 })
---
+++ src/modals/User.js
@@ -0,0 +1,26 @@
+const mangoose=require('mongoose');
+
+const userSchema=new mangoose.Schema({
+    firstName:{
+        type:String,
+    },
+    lastName:{
+        type:String,
+    },
+    email:{
+        type:String,
+    },
+    password:{
+        type:String,
+    },
+    age:{
+        type:Number,
+    },
+    gender:{
+        type:String,
+    }});
+
+    const User = mangoose.model('User', userSchema);
+
+    module.exports=User;
+
